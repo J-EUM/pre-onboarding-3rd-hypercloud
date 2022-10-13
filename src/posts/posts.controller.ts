a51c29b73179d94809e21baa99d0b8498fb683a4
J-EUM <eumjungmin1@gmail.com>
2022-10-14 00:53:00 +0900

    add: 예외처리(토큰x)

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,20 +6,22 @@ export class PostsController {
     constructor(private postService: PostService) {}
 
     @Post('/:id/like')
-    likePost(
-        @Body('userId', ParseIntPipe) userId: number,
-        @Param('id', ParseIntPipe) postId: number): void {
-        this.postService.likePost(userId, postId);
+    async likePost(
+        @Body('userId') userId: number,
+        @Param('id', ParseIntPipe) postId: number): Promise<any> {
+        await this.postService.likePost(userId, postId);
 
         return Object.assign({message: "SUCCESS"})
     }
 
     @Post('/:id/comment')
-    createComment(
+    async createComment(
         @Body('userId') userId: number,
-        @Param('id') postId: number,
+        @Param('id', ParseIntPipe) postId: number,
         @Body('text') text: string
-    ): void {
-        this.postService.createComment(userId, postId, text)
+    ): Promise<any> {
+        await this.postService.createComment(userId, postId, text)
+
+        return Object.assign({message: "SUCCESS"})
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
@@ -6,9 +6,18 @@ import { DataSource } from 'typeorm';
 export class PostService {
     constructor(@InjectDataSource() private readonly connection: DataSource) {}
 
-    async likePost(userId: number, postId: number): Promise<void> {
+    async likePost(userId: number, postId: number): Promise<any> {
 
-        const isExist = await this.connection.query(
+        const post: [] = await this.connection.query(
+            `SELECT * FROM posts 
+            WHERE id = ?
+            ;`, [postId]
+        );
+        if (!post.length) {
+            throw new NotFoundException("POST_NOT_EXIST");
+        }
+
+        const isExist: [] = await this.connection.query(
             `SELECT 1 FROM post_reaction 
             WHERE EXISTS (
                 SELECT * FROM post_reaction 
@@ -20,7 +29,6 @@ export class PostService {
                 `DELETE FROM post_reaction 
                 WHERE post_id = ? AND user_id = ?
                 ;`, [postId, userId]);
-            
         }
                 
         return await this.connection.query(
@@ -31,6 +39,15 @@ export class PostService {
     }
 
     async createComment(userId: number, postId: number, text: string): Promise<void> {
+        const post: [] = await this.connection.query(
+            `SELECT * FROM posts 
+            WHERE id = ?
+            ;`, [postId]
+        );
+        if (!post.length) {
+            throw new NotFoundException("POST_NOT_EXIST");
+        }
+
         return await this.connection.query(
             `INSERT INTO comments
             (post_id, user_id, text)
