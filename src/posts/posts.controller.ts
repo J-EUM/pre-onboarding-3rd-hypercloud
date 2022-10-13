d04f5850314b0888d96ddf43e85ab81380dd41d9
J-EUM <eumjungmin1@gmail.com>
2022-10-13 21:50:18 +0900

    add: 좋아요기능(토큰x)

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post, Res } from "@nestjs/common";
 import { PostService } from "./posts.service";
 
 @Controller('posts')
@@ -9,5 +9,14 @@ export class PostsController {
     @Get('test')
     getReaction(): Promise<any> {
         return this.postService.getReaction();
+    }
+
+    @Post('/like')
+    likePost(
+        @Body('userId') userId: number,
+        @Body('postId') postId: number): void {
+        this.postService.likePost(userId, postId);
+
+        return Object.assign({message: "SUCCESS"})
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -10,5 +10,29 @@ export class PostService {
         return this.connection.query('SELECT * FROM reactions;');
     }
 
+    async likePost(userId: number, postId: number): Promise<void> {
+
+        const isExist = await this.connection.query(
+            `SELECT 1 FROM post_reaction 
+            WHERE EXISTS (
+                SELECT * FROM post_reaction 
+                WHERE post_id = ? AND user_id = ?)
+            ;`, [postId, userId]);
+
+        if (isExist.length) {
+            return await this.connection.query(
+                `DELETE FROM post_reaction 
+                WHERE post_id = ? AND user_id = ?
+                ;`, [postId, userId]);
+            
+        }
+                
+        return await this.connection.query(
+            `INSERT INTO post_reaction
+            (post_id, user_id, reaction_id)
+            VALUES (?, ?, 1)
+            ;`, [postId, userId]);
+    }
+            
 }
 
