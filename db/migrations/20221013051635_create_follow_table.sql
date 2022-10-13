0af68be429cc7be6c7bf31ec55d8fde8d651271c
Ahnsang0915 <tmqjf4921@gmail.com>
2022-10-14 04:11:42 +0900

    modify: column name

--- db/migrations/20221013051635_create_follow_table.sql
+++ db/migrations/20221013051635_create_follow_table.sql
@@ -4,8 +4,8 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 following_user_id INT NOT NULL,
 followed_user_id INT NOT NULL,
 created_at DATETIME NOT NULL DEFAULT NOW(),
+FOREIGN KEY (follower_user_id) REFERENCES users (id),
 FOREIGN KEY (following_user_id) REFERENCES users (id),
-FOREIGN KEY (followed_user_id) REFERENCES users (id),
 UNIQUE KEY unique_follow (following_user_id,followed_user_id)
 );
 
